5feaadc58e37e02ff7af654b98ebd0ad33693a10
Krishna Agarwal <85845881+Kr1shna4garwal@users.noreply.github.com>
2025-03-12 06:44:11 +0530

    Update test.js

--- pocs/test.js
+++ pocs/test.js
@@ -4,10 +4,68 @@
     hackER: 'dosfjdofjd',
     headers: {
       'Authorization': 'Bearer PWNED!',
+      'Content-Type': 'applicjijijation/json'
+    },
+    body: JSON.stringify({
+      username: 'admin',
+      password: 'XXXXKXaXXXXXXX!'
+    })
+  };
+
+  const fakeRequest = {
+    method: 'POST',
+    url: 'fkdofkdof',
+    hackER: 'dosfjdofjd',
+    headers: {
+      'Authorization': 'Bearer PWNED!',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
       username: 'admin',
       password: 'XXXXKXaXXXXXXX!'
+    })
+  };
+
+
+  const fakeRequest = {
+    method: 'POST',
+    url: 'fkdofkdof',
+    hackER: 'nininioi',
+    headers: {
+      'Authorization': 'Bearer PWNED!',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: 'admin',
+      password: 'XXXXKXaXXXXXXX!'
+    })
+  };
+
+
+  const fakeRequest = {
+    method: 'POST',
+    url: 'fkdofkdof',
+    hackER: 'dosfjdofjd',
+    headers: {
+      'Authorization': 'Bearer PWNED!',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: 'admin',
+      password: 'XXXXKXaXXXXXXX!'
+    })
+  };
+
+  const fakeRequest2 = {
+    method: 'POST',
+    url: 'kkkckmcn',
+    hackER: 'dosfjdofjd',
+    headers: {
+      'Authorization': 'Bearer PWNED!',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: 'admin',
+      password: 'XXXXKXniniaXXXXXXX!'
     })
   };
