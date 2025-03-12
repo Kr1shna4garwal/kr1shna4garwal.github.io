e10a954547b6603eca6a811c5bf6227c3df2e722
Krishna Agarwal <85845881+Kr1shna4garwal@users.noreply.github.com>
2025-03-12 06:15:25 +0530

    Create test.js

---
+++ pocs/test.js
@@ -0,0 +1,13 @@
+  const fakeRequest = {
+    method: 'POST',
+    url: 'fkdofkdof',
+    hackER: 'dosfjdofjd',
+    headers: {
+      'Authorization': 'Bearer sk_test_FAKE_SECRET_TOKEN_ABC123',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: 'admin',
+      password: 'XXXXKXaXXXXXXX!'
+    })
+  };
